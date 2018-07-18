2aee53b0eaf5e991ccf9cdac47216e18bcaa76a4
Yoteichi <plonk@piano.email.ne.jp>
2018-07-18 23:24:09 +0900

    著作権表示を追加。

--- soundsystem2.js
+++ soundsystem2.js
@@ -1,3 +1,20 @@
+/*
+  This file is part of Mogeweb.
+
+  Mogeweb is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 2 of the License, or
+  (at your option) any later version.
+
+  Mogeweb is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with Mogeweb.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 var ht = Math.pow(2,1/12); // half tone
 
 class SoundSystem2 {
